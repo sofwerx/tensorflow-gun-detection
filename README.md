41ed0630aa290014e9a2def8d9a3ca5a4c23aac8
pushlog <dineshm.csit@gmail.com>
2018-03-05 18:17:04 -0600

    Added image resizer utility code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # tensorflow-gun-detection
+
+### utils/image_resizer.py
+  - usage
+    - python image_resizer.py -input=input_folder_images -output=output_folder -height=800 -width=600
---
+++ utils/image_resizer.py
@@ -0,0 +1,75 @@
+import argparse
+import glob
+import os
+
+import cv2
+from imutils import paths
+
+parser = argparse.ArgumentParser(description="Pass inputs")
+parser.add_argument("-input","--input_dir", required=True,
+    help="path to the input directory containing images")
+parser.add_argument("-output","--output_dir", required=True,
+    help="path to the output directory to store images")
+parser.add_argument("-height", "--height", required=True,
+    help="required height to resize")
+parser.add_argument("-width", "--width", required=True,
+    help="required width to resize")
+args = vars(parser.parse_args())
+
+required_height = int(args["height"])
+required_width = int(args["width"])
+input_path = args["input_dir"]
+output_path = args["output_dir"]
+
+# create output folder if it doesnot exist
+if not os.path.exists(output_path):
+    os.makedirs(output_path)
+
+def resize_image(image, width=None, height=None, inter=cv2.INTER_AREA):
+    """
+    resize the image without distorting it,
+    using the width, height cmds entered at command line.
+
+    interpolation docs = https://docs.opencv.org/3.0-beta/modules/imgproc/doc/geometric_transformations.html?highlight=resize#cv2.resize
+    """
+    dimensions = None
+    img_height, img_width, channels = image.shape
+
+    if width is None and height is None:
+        return image
+    elif width is None:
+        ratio = height / float(img_height)
+        dimensions = (int(img_width * ratio), height)
+    else:
+        ratio = width / float(img_width)
+        dimensions = (width, int(img_height * ratio))
+
+    resized_img = cv2.resize(image, dimensions, interpolation=inter)
+    return resized_img
+    # return cv2.resize(image, (required_width, required_height))
+
+
+for (dirpath, dirnames, image_names) in os.walk(input_path):
+    # remove images with invalid extension
+    image_names = sorted([img for img in image_names
+            if any([ext in img for ext in (".bmp", ".jpg", ".jpeg", ".png",
+                ".tif", ".tiff")]) ])
+
+    # loop through each image, get the dimensions
+    #      compare with the cmd height,width passed and resize.
+    for img_path in image_names:
+        imagepath = os.path.join(dirpath, img_path)
+
+        image = cv2.imread(imagepath)
+        try:
+            height, width, channels = image.shape
+            if height > required_height or width > required_width:
+                image = resize_image(image, required_width, required_height)
+            else:
+                image = resize_image(image)
+            # Write the image to output directory
+            img_out_path = os.path.join(output_path, img_path)
+
+            cv2.imwrite(img_out_path, image)
+        except AttributeError:
+            print("Cannot process image: ", img_path)
